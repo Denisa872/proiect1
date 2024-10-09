aac942d0deedc9c866d7221ef7d39871e570cbf6
Denisa872 <denisamihaila739@gmail.com>
2024-10-09 14:38:30 +0300

    Add initial main class

---
+++ hangman.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Define a simple function to print "Hello, world!"
+void say_hello(){
+    printf("Hello, World!\n");
+}
+
+//Main funtion
+int main(){
+    say_hello();
+    return 0;
+}
